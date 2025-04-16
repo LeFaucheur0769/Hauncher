4de1a8c635c4e0f2d3fa157be4b0d8d2cce8e7eb
LeFaucheur0769 <tristanburdin1@gmail.com>
2025-04-16 16:36:43 +0200

    modified:   launcher.py 	new file:   utils/cli/setVars.py 	new file:   utils/tools/nmap.py

---
+++ utils/cli/setVars.py
@@ -0,0 +1,11 @@
+def Lhost():
+    return input("Enter Lhost: ")
+
+def Lport():
+    return input("Enter Lport: ")
+
+def Rhost():
+    return input("Enter Rhost: ")
+
+def Rport():
+    return input("Enter Rport: ")
